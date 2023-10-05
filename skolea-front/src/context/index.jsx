9b1cc1049435a9e040408ba40346dea619082b62
Redwan G <redwan@macademia.fr>
2023-10-05 15:47:52 +0200

    ajoute des commentaires pour faciliter la comprehension du code

--- skolea-front/src/context/index.jsx
+++ skolea-front/src/context/index.jsx
@@ -1,35 +1,46 @@
-// Dans votre fichier UserContext.js
+// Importer les modules et dépendances nécessaires
 import { useEffect, useState } from "react";
 import { createContext } from 'react';
 import LoginServices from "../services/loginServices";
 
+// Créer un contexte (UserContext) pour gérer les données utilisateur
 export const UserContext = createContext();
 
+// Créer un composant UserProvider pour gérer les données utilisateur et l'état d'authentification
 export const UserProvider = ({ children }) => {
+  // Initialiser une variable d'état pour stocker les données utilisateur
   const [userData, setUserData] = useState(null);
 
+  // Fonction pour vérifier si l'utilisateur est authentifié
   const checkAuthentication = async () => {
+    // Récupérer le jeton d'authentification depuis le stockage local (localStorage)
     const token = localStorage.getItem('token');
+
+    // Si un jeton est trouvé, tenter de le vérifier en effectuant une requête au serveur
     if (token) {
       try {
-        
+        // Appeler une fonction de service pour vérifier le jeton et récupérer les données utilisateur
         const response = await LoginServices.whoAmI(token);
+
+        // Extraire les données utilisateur de la réponse et mettre à jour l'état
         const userData = response.userData;
         setUserData(userData);
-      
-
       } catch (error) {
-        console.error('Error during authentication check:', error);
+        // Gérer les erreurs qui surviennent lors de la vérification de l'authentification
+        console.error('Erreur lors de la vérification de l\'authentification :', error);
       }
     } else {
+      // Si aucun jeton n'est trouvé, définir les données utilisateur sur null (l'utilisateur n'est pas authentifié)
       setUserData(null);
     }
   };
 
+  // Utiliser le hook useEffect pour exécuter la vérification d'authentification lorsque le composant est monté
   useEffect(() => {
     checkAuthentication();
   }, []);
 
+  // Fournir les données utilisateur aux composants enfants via UserContext.Provider
   return (
     <UserContext.Provider value={{ userData }}>
       {children}
--- skolea-front/src/view/contact/ContactForm.jsx
+++ skolea-front/src/view/contact/ContactForm.jsx
@@ -1,55 +1,73 @@
-import React from 'react'
-import { Card } from 'reactstrap'
-import { Container } from '@mui/material'
+import React, { useState } from 'react';
+import { Card, Container } from '@mui/material';
+
 const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState('Send')
-  const onSubmit = (e) => {
-    e.preventDefault()
-    setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
+  // Utilisation du hook useState pour gérer l'état du formulaire
+  const [formStatus, setFormStatus] = useState('Send');
+
+  // Gestionnaire de soumission du formulaire
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // Mettre à jour le statut du formulaire pendant la soumission
+    setFormStatus('Submitting...');
+
+    try {
+      const { name, email, message } = e.target.elements;
+      // Création d'un objet avec les données du formulaire
+      const nameValue = name.value;
+      const emailValue = email.value;
+      const messageValue = message.value;
+  
+
+      // Ici, vous pouvez envoyer les données à votre backend ou effectuer une autre action requise
+      // par exemple, une requête HTTP pour enregistrer les données
+
+      // Une fois l'action effectuée avec succès, vous pouvez mettre à jour le statut du formulaire
+      setFormStatus('Submitted');
+    } catch (error) {
+      console.error('Erreur lors de la soumission du formulaire :', error);
+      // Gérer les erreurs qui peuvent survenir lors de la soumission du formulaire
+      setFormStatus('Error');
     }
-  }
+  };
+
   return (
     <>
-    <div style={{
-        marginTop:"750px"
-      }}></div>
-    
-    <Container >
-      <Card style={{ padding:"50px" }}>
-        <div className="container mt-5">
-          <h2 className="mb-3">contact</h2>
-          <form onSubmit={onSubmit}>
-            <div className="mb-3">
-              <label className="form-label" htmlFor="name">
-                Name
-              </label>
-              <input className="form-control" type="text" id="name" required />
-            </div>
-            <div className="mb-3">
-              <label className="form-label" htmlFor="email">
-                Email
-              </label>
-              <input className="form-control" type="email" id="email" required />
-            </div>
-            <div className="mb-3">
-              <label className="form-label" htmlFor="message">
-                Message
-              </label>
-              <textarea className="form-control" id="message" required />
-            </div>
-            <button className="btn btn-danger" type="submit">
-              {formStatus}
-            </button>
-          </form>
-        </div>
-      </Card>
-    </Container>
+      {/* Un espace vide pour déplacer le formulaire vers le haut */}
+      <div style={{ marginTop: '750px' }}></div>
+
+      <Container>
+        <Card style={{ padding: '50px' }}>
+          <div className="container mt-5">
+            <h2 className="mb-3">Contactez-nous</h2>
+            <form onSubmit={onSubmit}>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="name">
+                  Nom
+                </label>
+                <input className="form-control" type="text" id="name" required />
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="email">
+                  Email
+                </label>
+                <input className="form-control" type="email" id="email" required />
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="message">
+                  Message
+                </label>
+                <textarea className="form-control" id="message" required />
+              </div>
+              <button className="btn btn-danger" type="submit">
+                {formStatus}
+              </button>
+            </form>
+          </div>
+        </Card>
+      </Container>
     </>
-  )
-}
-export default ContactForm
+  );
+};
+
+export default ContactForm;
